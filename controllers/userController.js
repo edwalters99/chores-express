05b4cce88e20afc3e13e8c8f22d190c904484c10
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-21 21:04:23 +1000

    error handling / basic validation for register user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,30 @@
-const registerUser = (req, res) => {
-    res.send('Register Route');
-}
+const asyncHandler = require('express-async-handler'); // enables wrapping of controller functions so inside can use async/await
 
-const loginUser = (req, res) => {
+// @desc       Register a new user
+// @route      /api/users
+// @access     Public
+
+const registerUser = asyncHandler(async (req, res) => {
+    // console.log(req.body);
+    const {username, email, password, familyname, } = req.body; 
+
+    // Validation
+    if(!username || !email || !password || !familyname) {
+        res.status(400); // client error
+        throw new Error('Please include all fields');
+    };
+});
+
+
+// @desc       Login a user
+// @route      /api/users/login
+// @access     Public
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login Route');
-}
+})
 
 module.exports = {
     registerUser,
     loginUser
 };
+//exports used in UserRoutes
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const PORT = process.env.PORT || 3000; // this is so it works when hosted and lets the host provider assign a port for you.
 
 const app = express();
-app.use(cors()); //enables cors
+app.use(cors());
+app.use(express.json()); // so the server can accept raw json in post request
+app.use(express.urlencoded({extended: false})); // so the server can accept url encoded form data in post request
+
 
 app.get('/', (req, res) => {
     res.status(200).json({ message : 'Welcome to the Chores Api' });
 });
 
 //Routes
-app.use('/api/users', require('./routes/userRoutes'));   // connects endpoint to Routes
+app.use('/api/users', require('./routes/userRoutes'));   // connects userRoutes to /api/users.  Sub routes are then defined in userRoutes.
 
+app.use(errorHandler);  // custom error handler (middleware)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
