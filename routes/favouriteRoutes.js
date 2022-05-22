4a6aed59795ada17998fd695c784d1de04d046ba
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-22 22:05:46 +1000

    favourites UPDATE and DELETE working

--- routes/favouriteRoutes.js
+++ routes/favouriteRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getFavourites, createFavourite } = require('../controllers/favouriteController');
+const { getFavourites, createFavourite, updateFavourite, deleteFavourite } = require('../controllers/favouriteController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getFavourites).post(protect, createFavourite);
+router.route('/:id').put(protect, updateFavourite).delete(protect, deleteFavourite);
 
 module.exports = router;
